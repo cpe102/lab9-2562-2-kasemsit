ed94434d12f85216318dbb01ef90debd48683c25
Kasemsit Teeyapan <kasemsit@gmail.com>
2020-01-28 15:46:53 +0700

    done lab1and2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	ifstream f1("cheerbook.txt");
+	ofstream f2("cheerbook_copy.txt");
+	string s;
+	while (getline(f1,s)){		
+		f2 << s << "\n";
+	}
+	f1.close();
+	f2 << "-------------------- SOTUS ---------------------";
+	f2.close();
+	
+	return 0;
+}
+
 //cout << "-------------------- SOTUS ---------------------";
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	ifstream f1("score.txt");	
+	string s;
+	while (getline(f1,s)){		
+		f2 << s << "\n";
+	}
+	f1.close();
+	f2 << "-------------------- SOTUS ---------------------";
+	f2.close();
+	
+	return 0;
+}
+
+
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
